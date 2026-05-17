d4ad768903b4528f7e772e0cbfade3e341318068
sadafnazari1 <153479581+sadafnazari1@users.noreply.github.com>
2026-05-17 16:16:24 +0500

    UI display and hangman graphics module

--- sadaf_nazari_01-135232-084.asm
+++ sadaf_nazari_01-135232-084.asm
@@ -1,22 +1,19 @@
-;```asm
+
 ;MEMBER 2: SADAF NAZARI (01-135232-084)
 ;Module: UI/Display & Hangman Graphics
 
 include 'emu8086.inc'
 org 100h
 
-display_word    db 20 dup(0)
+display_word  db 20 dup(0)
 
 
-lives           db 6            
+       lives  db 6            
 
 ; Stores guessed letters flags (A-Z)
 guessed_letters db 26 dup(0)    
 
-
-; -------------------------------------------------
 ; Clears the screen and resets display mode
-; -------------------------------------------------
 clear_screen proc
     push ax
     mov ah, 0        
@@ -26,11 +23,8 @@ clear_screen proc
     ret
 clear_screen endp
 
-
-; -------------------------------------------------
 ; Draws the basic gallows structure
-; This stays visible during the game
-; -------------------------------------------------
+; This visible during the game
 draw_gallows_base proc
   
     gotoxy 0, 0
@@ -39,7 +33,7 @@ draw_gallows_base proc
     gotoxy 0, 1
     print '  |   |'
     
-    gotoxy 0, 2
+    gotoxy 0, 2      ; empty hanging space 
     print '      |'
     gotoxy 0, 3
     print '      |'
@@ -54,36 +48,33 @@ draw_gallows_base proc
     gotoxy 0, 8
     print '      |'
     
-    gotoxy 0, 9
+    gotoxy 0, 9       ; basic platform 
     print '======='
     ret
 draw_gallows_base endp
 
-
-; -------------------------------------------------
-; Updates hangman drawing according to lives lost
+; Updates hangman procedure 
 ; Each wrong guess adds a body part
-; -------------------------------------------------
 update_hangman proc
    
     mov al, 6
-    sub al, lives      
+    sub al, lives   ;al number of wrong guesses    
     
    
     cmp al, 0
-    je hangman_0     
+    je hangman_0    ; no parts  
     cmp al, 1
-    je hangman_1      
+    je hangman_1     ;head  
     cmp al, 2
-    je hangman_2        
+    je hangman_2     ;head and body    
     cmp al, 3
-    je hangman_3       
+    je hangman_3     ;left arm  
     cmp al, 4
-    je hangman_4    
+    je hangman_4      ;right 
     cmp al, 5
-    je hangman_5      
+    je hangman_5       ;left 
     cmp al, 6
-    je hangman_6        
+    je hangman_6       ;complete  
     ret
 
 hangman_0:
@@ -91,13 +82,13 @@ hangman_0:
     ret                
 
 hangman_1:
-    ; Draw head
+    ; Draw head circle 
     gotoxy 6, 2
     putc 'O'            
     ret
 
 hangman_2:
-    ; Draw body
+    ; Draw body virtical line 
     gotoxy 6, 2
     putc 'O'            
     gotoxy 6, 3
@@ -105,29 +96,29 @@ hangman_2:
     ret
 
 hangman_3:
-    ; Draw left arm
+             
     gotoxy 6, 2
     putc 'O'           
-    gotoxy 5, 3
+    gotoxy 5, 3  ;left arm
     putc '/'            
-    gotoxy 6, 3
+    gotoxy 6, 3   ;body 
     putc '|'          
     ret
 
 hangman_4:
-    ; Draw right arm
+ 
     gotoxy 6, 2
     putc 'O'           
     gotoxy 5, 3
     putc '/'           
     gotoxy 6, 3
     putc '|'            
-    gotoxy 7, 3
+    gotoxy 7, 3   ;right arm
     putc 92           
     ret
 
 hangman_5:
-    ;Draw left leg
+   
     gotoxy 6, 2
     putc 'O'            ; Head
     gotoxy 5, 3
@@ -140,8 +131,8 @@ hangman_5:
     putc '/'            ; Left leg
     ret
 
-hangman_6:
-    ; Final stage
+hangman_6:         ; Final stage
+   
     gotoxy 6, 2
     putc 'O'            ; Head
     gotoxy 5, 3
@@ -159,9 +150,9 @@ hangman_6:
 update_hangman endp
 
 ; Displays game information 
-display_status proc
+display_status proc ;display hidden word and space
    
-    gotoxy 15, 2
+    gotoxy 15, 2      
     print "Word: "
     lea si, display_word
     call print_string_with_spaces
@@ -192,13 +183,13 @@ psws_loop:
     cmp al, 0         
     je psws_done        ; Stop at null character
     
-    ; Print character then add space
+    ; Print character and space
     putc al             
     putc ' '            
     jmp psws_loop
     
 psws_done:
-    pop si              
+    pop si     ;restore pointer         
     ret
 print_string_with_spaces endp
 
@@ -208,10 +199,10 @@ print_string_with_spaces endp
 display_guessed_letters proc
     lea si, guessed_letters
     mov cx, 26          ; Totalletters
-    mov bl, 'A'         ; Startfrom A
+    mov bl, 'A'          ; Startfrom A
     
 dgl_loop:
-    mov al, [si]        ; Load guessed flag
+    mov al, [si]         ; Load guessed flag
     cmp al, 1          
     jne dgl_skip        ; Skipifnotguessed
     
@@ -220,7 +211,7 @@ dgl_loop:
     putc ' '            
     
 dgl_skip:
-    inc si              ; Move to next flag
+    inc si               ; Move to next array element 
     inc bl              ; Move to next letter
     loop dgl_loop       
     
